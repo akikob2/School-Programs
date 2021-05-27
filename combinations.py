cdf7264250446e79d494a3cd5cba07954dfbd260
Akiko Barreras <84736090+akikob2@users.noreply.github.com>
2021-05-27 15:24:56 -0500

    Add files via upload

---
+++ combinations.py
@@ -0,0 +1,41 @@
+#  File: Combinations.py
+#  Description: Getting the number of combinations for 0 to 52 cards
+#  Student's Name: Akiko Barreras
+#  Student's UT EID: ab63527
+#  Course Name: CS 303E 
+#  Unique Number: 50180
+#
+#  Date Created: 3/7/2020
+#  Date Last Modified: 3/7/2020
+
+def main():
+
+    #making factorial function
+    def factorial(num):
+        fact = 1
+        for i in range (1,num+1):
+            fact *= i
+        return fact
+
+    #combination function
+    def combinations(n,r):
+        x = factorial(n)
+        y = factorial(r)
+        z = factorial(n-r)
+        integer = x // (y*z)
+        return integer
+
+    #header
+    print()
+    print("Cards",end='')
+    print(format("Combinations",">17s"))
+    print("="*22)
+
+    #printing numbers of cards and their combinations
+    for i in range(0,53):
+        print(format(i,">3d"),end="")
+        print(format(combinations(52,i),">19d"))
+    print()
+
+
+main()
